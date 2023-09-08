064eb3a4ed8b8ca5843f9c3e64d10bd103a2ca63
Joon-Klaps <joon.klaps@kuleuven.be>
2023-09-08 13:26:18 +0000

    adding hist plot

--- bin/plot_bed_coverage.R
+++ bin/plot_bed_coverage.R
@@ -7,14 +7,19 @@ coverage=read.table(args[1], sep="\t", header=F)
 title= args[2]
 
 # renames the header
-coverage=rename(coverage,c("Chr"="V1", "locus"="V2", "depth"="V3")) 
+coverage=rename(coverage,c("Chr"="V1", "locus"="V2", "depth"="V3"))
 
-ggplot(coverage, aes(x=locus, y=depth)) +
+p1<-ggplot(coverage, aes(x=locus, y=depth)) +
     geom_point(colour="red", size=1, shape=20, alpha=1/3) +
-    scale_y_continuous(trans = scales::log10_trans(), breaks = scales::trans_breaks("log10", function(x) 10^x))
+    scale_y_continuous(trans = scales::log10_trans(), breaks = scales::trans_breaks("log10", function(x) 10^x))+
+    ggtitle("Genome-wide coverage")
 
-ggsave(paste(title,".pdf",sep=""), width = 20, height = 10, units="cm")
+p2<-ggplot(coverage, aes(x= depth)) +
+    geom_histogram(binwidth=1)+
+    ggtitle("Coverage distribution")
 
+plot<- ggarrange(p1, p2, ncol = 2, nrow = 1, )
+ggsave(plot, file= paste(title,".pdf",sep=""), width = 20, height = 10, units="cm")
 
 ### LOG files
 sink(paste(title, "R_session.log", sep = '.'))
